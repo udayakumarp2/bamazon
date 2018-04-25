8babc1c92c86e50f19e595bb7d8f96cafa8c2370
udayakumarp2 <31020465+udayakumarp2@users.noreply.github.com>
2018-04-25 16:36:43 -0400

    Add files via upload

---
+++ bamazon/Add SQL.sql
@@ -0,0 +1,17 @@
+USE bamazon;
+
+
+
+INSERT INTO products(itemid,productname, departmentname, price,stockquantity)
+VALUES (1, "bmw hoodie", "Clothing/Apparel", 50, 20 ),
+(2, "bmw hoodie", "Clothing/Apparel", 50, 20 ),
+(3, "mercedes hoodie", "Clothing/Apparel", 50, 20 ),
+(4, "audi hoodie", "Clothing/Apparel", 50, 20 ),
+(5, "ferrari hoodie", "Clothing/Apparel", 50, 20 ),
+(6, "toothbrush", "Healthcare", 6, 1000 ),
+(7, "shampoo", "Healthcare", 6, 1000 ),
+(8, "milk", "Produce", 3, 40 ),
+(9, "gold chain", "Accessories", 2000, 2 ),
+(10, "northface backpack", "Clothing/Apparel", 100, 20 ),
+(11, "paint", "Home Improvement", 10, 50 ),
+(12, "Gucci Mane album", "Music", 10, 40 );
---
+++ bamazon/README.md
@@ -0,0 +1,22 @@
+# bamazon
+This is my working client facing node.js application
+
+This is what happens when I client purchases a product.
+Total inventory is decreased and the user receives a message.
+![GitHub Logo](/purchasing.png)
+Format: ![Alt Text](url)
+----------------------------------------------------------
+
+This is what happens when a client enters a prodcut that doesn't exits. 
+
+![GitHub Logo](/NotWorking.png)
+Format: ![Alt Text](url)
+
+-----------------------------------------------------------
+This is what happens when the customer quits the application. 
+
+![GitHub Logo](/Quitting.png)
+Format: ![Alt Text](url)
+
+
+
